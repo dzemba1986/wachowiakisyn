fec94f8dab88b6431583b203edb164631ef81574
Daniel <daniel.mikolajewski@wachowiakisyn.pl>
2016-12-19 13:44:25 +0100

    Problem z dodawaniem MC do magazynu

--- backend/controllers/MediaConverterController.php
+++ backend/controllers/MediaConverterController.php
@@ -4,13 +4,13 @@ namespace backend\controllers;
 
 use Yii;
 use yii\widgets\ActiveForm;
-use backend\models\Camera;
+use backend\models\MediaConverter;
 
-class CameraController extends DeviceController
+class MediaConverterController extends DeviceController
 {	
 	public function actionValidation($id = null){
 		 
-		$modelDevice = is_null($id) ? new Camera() : $this->findModel($id);
+		$modelDevice = is_null($id) ? new MediaConverter() : $this->findModel($id);
 		
 		$request = Yii::$app->request;
 		
@@ -28,7 +28,7 @@ class CameraController extends DeviceController
     
     protected function findModel($id)
     {
-        if (($model = Camera::findOne($id)) !== null) {
+        if (($model = MediaConverter::findOne($id)) !== null) {
             return $model;
         } else {
             throw new NotFoundHttpException('The requested page does not exist.');
