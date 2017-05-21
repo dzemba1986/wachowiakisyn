fe80766b4cbe10944d619dc318c099446582b7c7
Daniel <daniel.mikolajewski@wachowiakisyn.pl>
2017-05-21 21:30:27 +0200

    Stowrzenie active model dla relacji "historia_ip"

---
+++ backend/models/HistoryIp.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace backend\models;
+
+/**
+ * This is the model class for table "history_ip".
+ *
+ * The followings are the available columns in table 'model':
+ * @property string $ip
+ * @property string $from_date
+ * @property string $to_date
+ * @property integer $address
+ */
+
+class HistoryIp extends \yii\db\ActiveRecord
+{
+	const SCENARIO_CREATE = 'create';
+	const SCENARIO_UPDATE = 'update';
+	
+	/**
+	 * @return string the associated database table name
+	 */
+	public static function tableName()
+	{
+		return 'history_ip';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		return [
+			['ip', 'required', 'message' => 'Wartość wymagana', 'on' => self::SCENARIO_CREATE],
+			
+			['from_date', 'required', 'message' => 'Wartość wymagana', 'on' => self::SCENARIO_CREATE],
+				
+			['to_date', 'required', 'message' => 'Wartość wymagana', 'on' => self::SCENARIO_UPDATE],
+				
+			['address', 'integer'],
+			['address', 'required', 'message' => 'Wartość wymagana', 'on' => self::SCENARIO_CREATE],
+				
+			[['ip', 'from_date', 'to_date', 'address'], 'safe'],
+		];
+	}
+	
+	public function scenarios(){
+		
+		$scenarios = parent::scenarios();
+		$scenarios[self::SCENARIO_CREATE] = ['ip', 'from_date', 'address'];
+		$scenarios[self::SCENARIO_UPDATE] = ['to_date'];
+		
+		return $scenarios;
+	}
+	
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'ip' => 'Adres IP',
+			'from_date' => 'Data od',
+			'to_date' => 'Data do',
+		);
+	}
+    
+    public function getModelAddress(){
+	
+		return $this->hasOne(Device::className(), ['address' => 'id']);
+	}
+}
