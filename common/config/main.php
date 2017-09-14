c768bd5fee99dea2eab548914eb3bb40497c8dc6
Daniel <daniel.mikolajewski@wachowiakisyn.pl>
2017-09-14 14:20:45 +0200

    Aliasy w aplikacji
    1. Konfiguracja aliasów w formie tablicy.
    2. Dodanie aliasu dla loganalyzera.

--- common/config/main.php
+++ common/config/main.php
@@ -6,4 +6,12 @@ return [
             'class' => 'yii\caching\FileCache',
         ],
     ],
+	//ustawienie dodatkowych aliasów do sciezek	
+	'aliases' => [
+		'@common' => dirname(__DIR__),
+		'@frontend' => dirname(dirname(__DIR__)) . '/frontend',
+		'@backend' => dirname(dirname(__DIR__)) . '/backend',
+		'@console' => dirname(dirname(__DIR__)) . '/console',
+		'@loganalyzer' => 'http://172.20.4.17:701'
+	]	
 ];
