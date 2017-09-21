a0943e7a96c6e1cc4c9ae435305bcfca20c70407
Daniel <daniel.mikolajewski@wachowiakisyn.pl>
2017-09-21 14:52:33 +0200

    Dokumentacja dla modelu 'Ip'

--- backend/models/Ip.php
+++ backend/models/Ip.php
@@ -2,33 +2,36 @@
 
 namespace backend\models;
 
-use Yii;
+use yii\db\ActiveQueryInterface;
+use yii\db\ActiveRecord;
 /**
  * This is the model class for table "ip".
  *
  * The followings are the available columns in table 'ip':
  * @property string $ip
- * @property integer $main
- * @property integer $subnet
+ * @property boolean $main
+ * @property integer $subnet 
  * @property integer $device
+ * @property ActiveQueryInterface $modelSubnet
+ * @property ActiveQueryInterface $modelDevice
+ * 
+ * @author Mikołajewski Daniel <daniel.mikolajewski@wachowiakisyn.pl>
  */
-class Ip extends \yii\db\ActiveRecord
+class Ip extends ActiveRecord
 {
 	/**
 	 * @return string the associated database table name
 	 */
-	public static function tableName()
-	{
+	public static function tableName() : string {
+		
 		return '{{ip}}';
 	}
 
 	/**
 	 * @return array validation rules for model attributes.
 	 */
-	public function rules()
-	{
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
+	public function rules() : array {
+		
 		return [
 			['ip', 'required', 'message' => 'Wartość wymagana'],
 			
@@ -36,7 +39,6 @@ class Ip extends \yii\db\ActiveRecord
 			['subnet', 'integer', 'message' => 'Wartość musi być liczbą'],
 				
 			// The following rule is used by search().
-			// @todo Please remove those attributes that should not be searched.
 			[['ip', 'main', 'subnet', 'device'], 'safe'],
 		];
 	}
@@ -44,25 +46,31 @@ class Ip extends \yii\db\ActiveRecord
 	/**
 	 * @return array customized attribute labels (name=>label)
 	 */
-	public function attributeLabels()
-	{
-		return array(
+	public function attributeLabels() : array {
+		
+		return [
 			'ip' => 'Adres IP',	
 			'main' => 'Główny',
 			'subnet' => 'Podsieć',
 			'device' => 'Urządzenie',
-		);
+		];
 	}
 	
-	public function getModelSubnet(){
+	/**
+	 * @return ActiceQueryInterface the relational query object
+	 */
+	public function getModelSubnet() : ActiveQueryInterface {
 	
-		//Connection ma tylko 1 Address
+		//adres ip należy do 1 podsieci
 		return $this->hasOne(Subnet::className(), ['id' => 'subnet']);
 	}
 	
-	public function getModelDevice(){
+	/**
+	 * @return ActiceQueryInterface the relational query object
+	 */
+	public function getModelDevice() : ActiveQueryInterface {
 	
-		//Connection ma tylko 1 Address
+		//adres ip należy do 1 urządzenia
 		return $this->hasOne(Device::className(), ['id' => 'device']);
 	}
 }
