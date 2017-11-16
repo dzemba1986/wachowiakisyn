be8da49f0f9af5a78ee644bb3b3ad9c7631f2c6b
Daniel <daniel.mikolajewski@wachowiakisyn.pl>
2017-11-16 13:17:56 +0100

    Zmiany w formularzu rejestracji i logowania

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -12,7 +12,7 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="site-login">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>Please fill out the following fields to login:</p>
+    <p>Aby się zalogować wypełni pola:</p>
 
     <div class="row">
         <div class="col-lg-5">
@@ -21,7 +21,7 @@ $this->params['breadcrumbs'][] = $this->title;
                 <?= $form->field($model, 'password')->passwordInput() ?>
                 <?= $form->field($model, 'rememberMe')->checkbox() ?>
                 <div style="color:#999;margin:1em 0">
-                    If you forgot your password you can <?= Html::a('reset it', ['site/request-password-reset']) ?>.
+                    Jeżeli zapomniałeś hasła możesz je <?= Html::a('zresetować', ['site/request-password-reset']) ?>.
                 </div>
                 <div class="form-group">
                     <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
