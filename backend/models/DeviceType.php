fe926b1ad8978afc3849f01285e301f7a4fe2767
Daniel <daniel.mikolajewski@wachowiakisyn.pl>
2017-11-16 10:51:12 +0100

    Dodanie funkcji `findOrderName`

--- backend/models/DeviceType.php
+++ backend/models/DeviceType.php
@@ -2,13 +2,10 @@
 
 namespace backend\models;
 
-use Yii;
-use yii\data\ActiveDataProvider;
 use backend\models\Device;
+use Yii;
+
 /**
- * This is the model class for table "tbl_package".
- *
- * The followings are the available columns in table 'tbl_package':
  * @property integer $id
  * @property string $name
  * @property boolean $list
@@ -17,9 +14,6 @@ use backend\models\Device;
  */
 class DeviceType extends \yii\db\ActiveRecord
 {
-	/**
-	 * @return string the associated database table name
-	 */
 	public static function tableName()
 	{
 		return '{{device_type}}';
@@ -27,16 +21,11 @@ class DeviceType extends \yii\db\ActiveRecord
 
 	public function getModelDevice(){
 	
-		//Wiele instalacji na danym adresie
 		return $this->hasMany(Device::className(), ['type'=>'id']);
 	}
-	/**
-	 * @return array validation rules for model attributes.
-	 */
+	
 	public function rules()
 	{
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
 		return [
 			[['name'], 'required'],
 			[['list', 'children'], 'boolean'],	
@@ -53,5 +42,10 @@ class DeviceType extends \yii\db\ActiveRecord
 			'id' => 'ID',
 			'name' => 'Nazwa',
 		);
+	}
+	
+	public static function findOrderName(){
+		
+		return self::find()->select(['id', 'name'])->orderBy('name');
 	}
 }
