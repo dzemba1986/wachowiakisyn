ca8dae2b516680c4a4bf127a426eed58204ea2b9
Daniel <daniel.mikolajewski@wachowiakisyn.pl>
2017-12-07 08:51:18 +0100

    Poprawki w skryptach dla voipa #75

--- backend/views/device/_script_gateway_voip.php
+++ backend/views/device/_script_gateway_voip.php
@@ -2,87 +2,92 @@
 
 use backend\models\Device;
 
-
 $parent = $modelDevice->modelTree[0]->parent_device;	//id parent
 $parentPortIndex = $modelDevice->modelTree[0]->parent_port; //index port
+$portNumber = $parentPortIndex + 1;
 
 $modelDeviceParent = Device::findOne($parent);
 $arPortsParent = $modelDeviceParent->modelModel->port;
-// var_dump($arPortsParent);
 
 if($modelDeviceParent->modelModel->config == 1){
 
-$add = 'ip access-list voip' . ($parentPortIndex + 1) . '
-deny-udp any any any 68' . '
-permit any ' . $modelDevice->modelIps[0]->ip . ' 0.0.0.0 213.5.208.0 0.0.0.63' . '
-permit any ' . $modelDevice->modelIps[0]->ip . ' 0.0.0.0 213.5.208.128 0.0.0.63' . '
-permit any ' . $modelDevice->modelIps[0]->ip . ' 0.0.0.0 10.111.0.0 0.0.255.255' . '
-permit-udp 0.0.0.0 0.0.0.0 68 any 67' . '	
-exit' . '
-interface ethernet ' . $arPortsParent[$parentPortIndex] . '
-shutdown' . '		
-switchport trunk allowed vlan remove all' . '
-switchport mode access' . '
-description ' . $modelDevice->name . '
-switchport access vlan ' . $modelDevice->modelIps[0]->modelSubnet->modelVlan->id . '		
-spanning-tree portfast' . '
-spanning-tree bpduguard' . '
-service-acl input voip' . ($parentPortIndex + 1) . '		
-no shutdown' . '
-exit' . ' 
-exit' . '
-copy r s' . '
-y' . '
-';		
-		
-$delete = 'interface ethernet ' . $arPortsParent[$parentPortIndex] . '
-shutdown' . '		
-switchport access vlan 555' . '		
-no service-acl input' . '
-no shutdown' . '
-exit' . '
-no ip access-list voip' . ($parentPortIndex + 1) . '
-no ip access-list voip' . ($parentPortIndex + 1) . '		
-exit' . '
-copy r s' . '
-y' . '
-';		
+$add = <<<ADD
+ip access-list voip {$portNumber}
+deny-udp any any any 68
+permit any {$modelDevice->modelIps[0]->ip} 0.0.0.0 213.5.208.0 0.0.0.63
+permit any {$modelDevice->modelIps[0]->ip} 0.0.0.0 213.5.208.128 0.0.0.63
+permit any {$modelDevice->modelIps[0]->ip} 0.0.0.0 10.111.0.0 0.0.255.255
+permit-udp 0.0.0.0 0.0.0.0 68 any 67
+exit
+interface ethernet {$arPortsParent[$parentPortIndex]}
+shutdown
+switchport trunk allowed vlan remove all
+switchport mode access
+description {$modelDevice->name}
+switchport access vlan {$modelDevice->modelIps[0]->modelSubnet->modelVlan->id}
+spanning-tree portfast
+spanning-tree bpduguard
+service-acl input voip{$portNumber}
+no shutdown
+exit
+exit
+copy r s
+y
 
+ADD;
+
+$delete = <<<DELETE
+interface ethernet {$arPortsParent[$parentPortIndex]}
+shutdown
+switchport access vlan 555
+no service-acl input
+no shutdown
+exit
+no ip access-list voip{$portNumber}
+no ip access-list voip{$portNumber}
+exit
+copy r s
+y
+
+DELETE;
 }
 elseif($modelDeviceParent->modelModel->config == 2){
-	
-$add = 'access-list hardware voip' . ($parentPortIndex + 1) . '
-deny udp any any eq 68' . '
-permit ip ' . $modelDevice->modelIps[0]->ip . ' 0.0.0.0 213.5.208.0 0.0.0.63' . '
-permit ip ' . $modelDevice->modelIps[0]->ip . ' 0.0.0.0 213.5.208.128 0.0.0.63' . '
-permit ip ' . $modelDevice->modelIps[0]->ip . ' 0.0.0.0 10.111.0.0 0.0.255.255' . '
-permit udp 0.0.0.0 0.0.0.0 eq 68 any eq 67' . '		
-deny ip any any' . '
-exit' . '		
-interface ' . $arPortsParent[$parentPortIndex] . '		
-shutdown' . '		
-description ' . $modelDevice->name . '		
-switchport access vlan ' . $modelDevice->modelIps[0]->modelSubnet->modelVlan->id . '	
-access-group voip' . ($parentPortIndex + 1) . '		
-spanning-tree portfast' . '
-spanning-tree portfast bpdu-guard enable' . '		
-no shutdown' . '
-exit' . '		
-exit' . '
-wr' . '
-';		
 
-$delete = 'interface ' . $arPortsParent[$parentPortIndex] . '
-shutdown' . '
-no access-group voip' . ($parentPortIndex + 1) . '		
-switchport access vlan 555' . '
-no shutdown' . '		
-exit' . '		
-no access-list hardware voip' . ($parentPortIndex + 1) . '		
-exit' . '
-wr' . '
-';
+$add = <<<ADD
+access-list hardware voip{$portNumber}
+deny udp any any eq 68
+permit ip {$modelDevice->modelIps[0]->ip} 0.0.0.0 213.5.208.0 0.0.0.63
+permit ip {$modelDevice->modelIps[0]->ip} 0.0.0.0 213.5.208.128 0.0.0.63
+permit ip {$modelDevice->modelIps[0]->ip} 0.0.0.0 10.111.0.0 0.0.255.255
+permit udp 0.0.0.0 0.0.0.0 eq 68 any eq 67
+deny ip any any
+exit
+interface {$arPortsParent[$parentPortIndex]}
+shutdown
+description {$modelDevice->name}
+switchport access vlan {$modelDevice->modelIps[0]->modelSubnet->modelVlan->id}
+access-group voip{$portNumber}
+spanning-tree portfast
+spanning-tree portfast bpdu-guard enable
+no shutdown
+exit
+exit
+wr
 
+ADD;
+
+$delete = <<<DELETE
+interface {$arPortsParent[$parentPortIndex]}
+shutdown
+no access-group voip{$portNumber}		
+switchport access vlan 555
+no shutdown
+exit
+no access-list hardware voip{$portNumber}		
+exit
+wr
+
+DELETE;
 }
 ?>
 
