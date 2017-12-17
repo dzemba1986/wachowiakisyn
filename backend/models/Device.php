d29f78b71f0ca19054da263fc94b922a461f7e29
Daniel <daniel.mikolajewski@wachowiakisyn.pl>
2017-12-17 21:55:39 +0100

    Poprawki modelu `Device` c.d.

--- backend/models/Device.php
+++ backend/models/Device.php
@@ -2,8 +2,8 @@
 
 namespace backend\models;
 
-use yii\db\Query;
 use yii\db\ActiveRecord;
+
 /**
  * @property integer $id
  * @property boolean $status
@@ -33,6 +33,7 @@ class Device extends ActiveRecord
 	        'id',
 	        'status',
 	        'name',
+	        'proper_name',
 	        'desc',
 	        'address_id',
 	        'type_id'
@@ -76,6 +77,8 @@ class Device extends ActiveRecord
             ['status', 'required', 'message' => '{attribute} jest wymagany', 'on' => [self::SCENARIO_TOSTORE, self::SCENARIO_TOTREE]],
                       
             ['name', 'string', 'min' => 3, 'max' => 20],
+		    
+		    ['proper_name', 'string', 'min' => 3, 'max' => 30],
 				
             ['desc', 'string'],
             
@@ -85,110 +88,39 @@ class Device extends ActiveRecord
             ['type_id', 'integer'],
             ['type_id', 'required', 'message' => 'Wartość wymagana'],           
 				
-			[['status', 'desc', 'address_id', 'type_id', 'name'], 'safe'],
+			[['status', 'desc', 'address_id', 'type_id', 'name', 'proper_name'], 'safe'],
 		];
 	}
     
 	public function scenarios()
 	{
 		$scenarios = parent::scenarios();
-		$scenarios[self::SCENARIO_CREATE] = ['desc', 'address', 'type'];
-		$scenarios[self::SCENARIO_UPDATE] = ['status', 'name', 'desc', 'original_name'];
-		$scenarios[self::SCENARIO_TOSTORE] = ['address', 'status'];
-		$scenarios[self::SCENARIO_TOTREE] = ['address', 'status'];
+		$scenarios[self::SCENARIO_CREATE] = ['desc', 'type_id'];
+		$scenarios[self::SCENARIO_UPDATE] = ['name', 'desc', 'proper_name', 'address_id', 'status'];
 			
 		return $scenarios;
 	}
 	
-    public function beforeValidate()
-    {
-        if (parent::beforeValidate()) {
-
-            if(!empty($this->serial))   
-                $this->serial = strtoupper($this->serial);
-            
-            if(!empty($this->mac))
-              	$this->mac = strtolower($this->mac);
-
-            return true;
-        }
-    }
-
-		/**
-	 * @return array relational rules.
-	 */
-	public function getModelAddress(){
-	
-		//urządzenie ma jeden adres
-		return $this->hasOne(Address::className(), ['id'=>'address']);
-	}
-    
-    public function getModelIps(){
-	
-		//urządzenie ma wiele aresów IP
-		return $this->hasMany(Ip::className(), ['device'=>'id'])->orderBy(['main' => SORT_DESC]);
-	}
-        
-    public function getModelType(){
-	
-		//urządzenie ma jeden typ
-		return $this->hasOne(DeviceType::className(), ['id'=>'type']);
-	}
-    
-    public function getModelModel(){
-	
-		//urządzenie ma jeden typ
-		return $this->hasOne(Model::className(), ['id'=>'model']);
-	}
-    
-    public function getModelManufacturer(){
-	
-		//urządzenie ma jeden typ
-		return $this->hasOne(Manufacturer::className(), ['id'=>'manufacturer']);
-	}
-    
-    public function getModelTree(){
-	
-		//urządzenie ma jeden typ
-		return $this->hasMany(Tree::className(), ['device'=>'id']);
-	}
-    
-	/**
-	 * @return array customized attribute labels (name=>label)
-	 */
-    
 	public function attributeLabels()
 	{
 		return array(
 			'id' => 'ID',
 			'status' => 'Status',
             'name' => 'Nazwa',
-			'original_name' => 'Org.',	
+			'proper_name' => 'Nazwa własna',	
             'desc' => 'Opis',
-			'address' => 'Adres',
-			'type' => 'Typ',
-			'manufacturer' => 'Producent'	
+			'address_id' => 'Adres',
+			'type_id' => 'Typ',
 		);
 	}
 	
-	public static  function getDeviceList(){
-		
-		$query = new Query();
-		$query->select(['d.id', 'a.t_ulica', new \yii\db\Expression("
-	    		CASE
-	    			WHEN pietro IS NULL THEN
-	    				CONCAT(adrs.name, ' ', dom, dom_szczegol, ' ', '[', ip, ']')
-	    			ELSE
-	    				CONCAT(adrs.name, ' ', dom, dom_szczegol, ' (piętro', pietro, ')', ' ', '[', ip, ']')
-	    		END
-	    	")])
-    	->from('device d')
-    	->join('INNER JOIN', 'address a', 'a.id = d.address')
-    	->join('INNER JOIN', 'ip', 'ip.device = d.id')
-    	->join('INNER JOIN', 'address_short adrs', 'adrs.t_ulica = a.t_ulica')
-    	->where(['status' => true]);
-    	$command = $query->createCommand();
-    	
-    	return $command->queryAll();
+	public function getAddress(){
+	    
+	    return $this->hasOne(Address::className(), ['id' => 'address_id']);
+	}
+	
+	public function getType(){
+	    
+	    return $this->hasOne(DeviceType::className(), ['id' => 'type_id']);
 	}
 }
