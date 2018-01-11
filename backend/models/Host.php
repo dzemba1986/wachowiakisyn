5957b8ee24fce625ed05b02e10252387e816394a
Daniel <daniel.mikolajewski@wachowiakisyn.pl>
2018-01-11 07:18:20 +0100

    Poprawki w modelu hosta

--- backend/models/Host.php
+++ backend/models/Host.php
@@ -2,61 +2,59 @@
 
 namespace backend\models;
 
-use backend\models\Address;
-use backend\models\Model;
 use yii\helpers\ArrayHelper;
-use vakorovin\yii2_macaddress_validator\MacaddressValidator;
 
 /**
- * This is the model class for table "device".
- *
- * The followings are the available columns in table 'device':
  * @property integer $id
- * @property integer $status
+ * @property boolean $status
  * @property string $name
- * @property integer $mac
+ * @property string $proper_name
  * @property string $desc
- * @property integer $address
- * @property integer $type
+ * @property integer $address_id
+ * @property integer $type_id
+ * @property integer $mac
+ * @property string $start_date
+ * @property Address $address
+ * @property Type $type
  */
 
-class Host extends Device
-{
+class Host extends Device{
+    
 	const TYPE = 5; //id w tabeli device_type dla Hosta
 	
-	public function init()
-	{
-		$this->type = self::TYPE;
+	public function init(){
+		
+	    $this->type_id = self::TYPE;
 		parent::init();
 	}
 	
-	public static function find()
-	{
-		return new DeviceQuery(get_called_class(), ['type' => self::TYPE]);
+	public function attributes(){
+	    
+	    return ArrayHelper::merge(
+	        parent::attributes(),
+	        [
+	            'start_date',
+	        ]
+	    );
 	}
 	
-	public function beforeSave($insert)
-	{
-		$this->type = self::TYPE;
+	public static function find(){
+		
+	    return new DeviceQuery(get_called_class(), ['type_id' => self::TYPE]);
+	}
+	
+	public function beforeSave($insert){
+		
+	    $this->type_id = self::TYPE;
 		return parent::beforeSave($insert);
 	}
 
-	/**
-	 * @return array validation rules for model attributes.
-	 */
 	public function rules(){
 		
         return ArrayHelper::merge(
             parent::rules(),
             [
-            	['mac', 'filter', 'filter' => function($value) { return strtolower($value); }],
-            	['mac', 'string', 'min'=>12, 'max'=>17, 'tooShort'=>'Za mało znaków', 'tooLong'=>'Za dużo znaków'],
-            	['mac', 'required', 'message'=>'Wartość wymagana'],
-            	['mac', MacaddressValidator::className(), 'message'=>'Zły format'],
-            	['mac', 'unique', 'targetClass' => 'backend\models\Host', 'message' => 'Mac zajęty', 'when' => function ($model, $attribute) {
-            		return strtolower($model->{$attribute}) !== strtolower($model->getOldAttribute($attribute));
-            	}],
-            	['mac', 'trim', 'skipOnEmpty' => true],
+                ['mac', 'required', 'message' => 'Wartość wymagana'],
             	
                 [['mac'], 'safe'],
             ]
@@ -68,25 +66,22 @@ class Host extends Device
 		$scenarios = parent::scenarios();
 		$scenarios[self::SCENARIO_CREATE] = ArrayHelper::merge($scenarios[self::SCENARIO_CREATE], ['mac']);
 		$scenarios[self::SCENARIO_UPDATE] = ArrayHelper::merge($scenarios[self::SCENARIO_UPDATE], ['mac']);
-		$scenarios[self::SCENARIO_TOSTORE] = ArrayHelper::merge($scenarios[self::SCENARIO_TOSTORE], ['address', 'status']);
-		$scenarios[self::SCENARIO_TOTREE] = ArrayHelper::merge($scenarios[self::SCENARIO_TOTREE], ['address', 'status']);
-		//$scenarios[self::SCENARIO_DELETE] = ['close_date', 'close_user'];
 			
 		return $scenarios;
 	}
-    
-	/**
-	 * @return array customized attribute labels (name=>label)
-	 */
     
 	public function attributeLabels()
 	{
         return ArrayHelper::merge(
             parent::attributeLabels(),
             [
-                'layer3' => 'Warstwa',
-                'distribution' => 'Rodzaj',
+                'start_date' => 'Konfiguracja',
             ]
         ); 
+	}
+	
+	public function getIps(){
+	    
+	    return $this->hasMany(Ip::className(), ['device' => 'id'])->orderBy(['main' => SORT_DESC]);
 	}
 }
