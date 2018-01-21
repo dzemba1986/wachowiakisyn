e494cdb174b6525a62c1415f4af1052e854f8818
Daniel <daniel.mikolajewski@wachowiakisyn.pl>
2018-01-21 10:04:47 +0100

    Poprawki w modelu `Installation`

--- backend/models/Installation.php
+++ backend/models/Installation.php
@@ -2,12 +2,7 @@
 
 namespace backend\models;
 
-use Yii;
-use yii\db\Expression;
-
 /**
- * This is the model class for table "{{%installation}}".
- *
  * @property integer $id
  * @property integer $address
  * @property integer $wire_length
@@ -15,7 +10,7 @@ use yii\db\Expression;
  * @property string $socket_date
  * @property string $wire_user
  * @property string $socket_user
- * @property integer $type
+ * @property integer $type_id
  * @property string $invoice_date
  * @property string $status
  */
@@ -24,70 +19,54 @@ class Installation extends \yii\db\ActiveRecord
 	const SCENARIO_CREATE = 'create';
 	const SCENARIO_UPDATE = 'update';
 	const SCENARIO_SOCKET = 'socket';
-    /**
-     * @inheritdoc
-     */
+
     public static function tableName()
     {
         return '{{installation}}';
     }
 
-    /**
-     * @inheritdoc
-     */
 	public function rules(){
 		
 		return [
 			
-            ['wire_date', 'date', 'format' => 'yyyy-MM-dd', 'message'=>'Zły format'],
-            ['wire_date', 'match', 'pattern' => '/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/', 'message'=>'Zły format'],
-//             ['wire_date', 'default', 'value' => new Expression('NOW()'), 'on' => self::SCENARIO_CREATE],
-            ['wire_date', 'required', 'message'=>'Wartość wymagana', 'on' => self::SCENARIO_CREATE],
+            ['wire_date', 'date', 'format' => 'yyyy-MM-dd', 'message' => 'Zły format'],
+            ['wire_date', 'match', 'pattern' => '/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/', 'message' => 'Zły format'],
+            ['wire_date', 'required', 'message' => 'Wartość wymagana', 'on' => self::SCENARIO_CREATE],
             
-            ['socket_date', 'date', 'format' => 'yyyy-MM-dd', 'message'=>'Zły format'],
-            ['socket_date', 'match', 'pattern' => '/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/', 'message'=>'Zły format'],
+            ['socket_date', 'date', 'format' => 'yyyy-MM-dd', 'message' => 'Zły format'],
+            ['socket_date', 'match', 'pattern' => '/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/', 'message' => 'Zły format'],
             ['socket_date', 'default', 'value' => NULL, 'on' => [self::SCENARIO_CREATE, self::SCENARIO_UPDATE]],
-			['socket_date', 'required', 'message'=>'Wartość wymagana', 'on' => self::SCENARIO_SOCKET],	
+			['socket_date', 'required', 'message' => 'Wartość wymagana', 'on' => self::SCENARIO_SOCKET],	
             
-            ['invoice_date', 'date', 'format' => 'yyyy-MM-dd', 'message'=>'Zły format'],
-            ['invoice_date', 'match', 'pattern' => '/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/', 'message'=>'Zły format'],
+            ['invoice_date', 'date', 'format' => 'yyyy-MM-dd', 'message' => 'Zły format'],
+            ['invoice_date', 'match', 'pattern' => '/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/', 'message' => 'Zły format'],
             ['invoice_date', 'default', 'value' => NULL],
             
-            ['wire_length', 'integer', 'message'=>'Wymagana liczba'],
-            ['wire_length', 'required', 'message'=>'Wartość wymagana', 'on' => self::SCENARIO_CREATE],
+            ['wire_length', 'integer', 'message' => 'Wymagana liczba'],
+            ['wire_length', 'required', 'message' => 'Wartość wymagana', 'on' => self::SCENARIO_CREATE],
             
-            //['wire_user', 'string', 'min' => 3, 'tooShort' => 'Minimum 3 znaki'],
-			['wire_user', 'required', 'message'=>'Wartość wymagana', 'on' => self::SCENARIO_CREATE],
-			//['wire_user', 'each', 'rule' => ['string']],
+			['wire_user', 'required', 'message' => 'Wartość wymagana', 'on' => self::SCENARIO_CREATE],
             
-//             ['socket_user', 'string', 'min' => 3, 'tooShort'=>'Minimum 3 znaki'],
 			['socket_user', 'required', 'on' => self::SCENARIO_SOCKET],
 				
-            ['type', 'integer'],
-            ['type', 'required', 'message'=>'Wartość wymagana'],
+            ['type_id', 'integer'],
+            ['type_id', 'required', 'message' => 'Wartość wymagana'],
             
-            ['address', 'integer'],
-            ['address', 'required', 'message'=>'Wartość wymagana'],
+            ['address_id', 'integer'],
+            ['address_id', 'required', 'message' => 'Wartość wymagana'],
 				
 			['status', 'boolean'],
 			['status', 'default', 'value' => true],
-			['status', 'required', 'message'=>'Wartość wymagana'],
+			['status', 'required', 'message' => 'Wartość wymagana'],
             
-			// The following rule is used by search().
-			// @todo Please remove those attributes that should not be searched.
-			[
-				['address', 'wire_length', 'type', 'status',
-			  	'wire_date', 'socket_date',
-				'wire_user', 'socket_user', 'invoice_date'], 		
-				'safe'
-			],
+			[['address_id', 'wire_length', 'type_id', 'status', 'wire_date', 'socket_date', 'wire_user', 'socket_user', 'invoice_date'], 'safe'],
 		];
 	}
 	
 	public function scenarios()
 	{
 		$scenarios = parent::scenarios();
-		$scenarios[self::SCENARIO_CREATE] = ['id', 'wire_date', 'wire_length', 'wire_user', 'type', 'address'];
+		$scenarios[self::SCENARIO_CREATE] = ['id', 'wire_date', 'wire_length', 'wire_user', 'type_id', 'address_id'];
 		$scenarios[self::SCENARIO_UPDATE] = ['wire_date', 'wire_length', 'wire_user', 'socket_user', 'socket_date', 'invoice_date', 'status'];
 		$scenarios[self::SCENARIO_SOCKET] = ['socket_date', 'socket_user'];
 		 
@@ -101,13 +80,13 @@ class Installation extends \yii\db\ActiveRecord
 	{
 		return array(
 			'id' => 'ID',
-			'address' => 'Adres',
+			'address_id' => 'Adres',
 			'wire_length' => 'Długość',
 			'wire_date' => 'Kabel',
 			'socket_date' => 'Gniazdo',
 			'wire_user' => 'Monter kabla',
 			'socket_user' => 'Monter gniazda',
-			'type' => 'Usługa',
+			'type_id' => 'Usługa',
 			'invoice_date' => 'Zaksęgowano',
 			'street' => 'Ulica',
 			'house' => 'Blok',
@@ -118,21 +97,13 @@ class Installation extends \yii\db\ActiveRecord
 		);
 	}
     
-	/**
-	 * @return \yii\db\ActiveQuery
-	 */
-    public function getModelAddress(){
+    public function getAddress(){
     
-    	//Installation jest robiona na jednym Address
-    	return $this->hasOne(Address::className(), ['id'=>'address']);
+    	return $this->hasOne(Address::className(), ['id' => 'address_id']);
     }
     
-    /**
-     * @return \yii\db\ActiveQuery
-     */
-    public function getModelType(){
+    public function getType(){
     
-    	//Installation jest robiona w jednym Type
-    	return $this->hasOne(Type::className(), ['id'=>'type']);
+    	return $this->hasOne(Type::className(), ['id' => 'type_id']);
     }
 }
