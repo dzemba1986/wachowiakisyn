331f2769a8e0a04cb219c7d52e3217af341ba698
Daniel <daniel.mikolajewski@wachowiakisyn.pl>
2018-01-21 09:49:34 +0100

    Poprawki w modelu wyszukiwania

--- backend/models/HistoryIpSearch.php
+++ backend/models/HistoryIpSearch.php
@@ -9,21 +9,18 @@ class HistoryIpSearch extends HistoryIp
 	public function rules()
 	{
 		return [
-		// The following rule is used by search().
-		// @todo Please remove those attributes that should not be searched.
-		[
-            ['ip'], 'safe'],
-		];
+            ['ip', 'safe'],
+        ];
 	}
 	
 	public function search($params)
 	{
-		$query = HistoryIp::find()->joinWith('modelAddress');
+		$query = HistoryIp::find()->joinWith('address');
 	
 		$dataProvider = new ActiveDataProvider([
-				'query' => $query,
-				'pagination' => ['defaultPageSize' => 100, 'pageSizeLimit' => [1,5000]],
-				]);	
+			'query' => $query,
+			'pagination' => ['defaultPageSize' => 100, 'pageSizeLimit' => [1,5000]],
+		]);	
 		
 		if (!($this->load($params) && $this->validate())) {
 			return $dataProvider;
