df2fb8db5e2afdb9e37404868b8f999b3f4d04cd
Daniel <daniel.mikolajewski@wachowiakisyn.pl>
2018-01-31 09:04:16 +0100

    Dodano zdarzenia wywoływane po dodaniu i przed usunięciem ip
    Kiedy serwis doda nowego ip do urządzenia wywoływane jest zdarzenie
    które w historii ip zapisuje noewgo ip'ka. To samo dzieje się kiedy z
    urządzenia znika ip.

--- backend/models/Ip.php
+++ backend/models/Ip.php
@@ -15,21 +15,11 @@ use yii\db\ActiveRecord;
 
 class Ip extends ActiveRecord
 {
-    const EVENT_NEW_IP = 'new-ip';
-    const EVENT_DELETE_IP = 'delete-ip';
-    
 	public static function tableName() : string {
 		
 		return '{{ip}}';
 	}
 	
-	public function init() {
-	    
-	    parent::init();
-	    $this->on(self::EVENT_NEW_IP, ['backend\models\HistoryIp', 'createIp']);
-	    $this->on(self::EVENT_DELETE_IP, ['backend\models\HistoryIp', 'deleteIp']);
-	}
-
 	public function rules() : array {
 		
 		return [
@@ -64,5 +54,53 @@ class Ip extends ActiveRecord
 	public function getDevice() {
 	
 		return $this->hasOne(Device::className(), ['id' => 'device_id']);
+	}
+	
+	public function afterSave($insert, $changedAttributes) {
+	    
+	    //FIXME należy dodać ograniczenie na hosty/podsieci które chcemy rejestrować w historii
+	    if ($insert) {
+    	    $device = Device::findOne($this->device_id);
+    	    
+    	    $historyIp = new HistoryIp();
+    	    $historyIp->scenario = HistoryIp::SCENARIO_CREATE_IP;
+    	    $historyIp->ip = $this->ip;
+    	    $historyIp->from_date = date('Y-m-d H:i:s');
+    	    $historyIp->address_id = $device->address_id;
+    	    
+    	    try {
+    	        if (!$historyIp->save()) throw new \Exception('Problem z zapisem historii IP - dodanie IP');
+    	        
+    	        return true;
+    	    } catch (\Throwable $t) {
+    	        var_dump($historyIp->errors);
+    	        var_dump($t->getMessage());
+    	        exit();
+    	    }
+	    }
+	}
+	
+	public function beforeDelete() {
+	    
+	    if (!parent::beforeDelete()) {
+	        return false;
+	    }
+	    
+	    //FIXME należy dodać ograniczenie na hosty/podsieci które chcemy rejestrować w historii
+	    $device = Device::findOne($this->device_id);
+	    
+	    $historyIp = HistoryIp::findOne(['ip' => $this->ip, 'address_id' => $device->address_id, 'to_date' => null]);
+	    $historyIp->scenario = HistoryIp::SCENARIO_DELETE_IP;
+	    $historyIp->to_date = date('Y-m-d H:i:s');
+	    
+	    try {
+	        if (!$historyIp->save()) throw new \Exception('Problem z zapisem historii IP - usunięcie IP');
+	        
+	        return true;
+	    } catch (\Throwable $e) {
+	        var_dump($historyIp->errors);
+	        var_dump($t->getMessage());
+	        exit();
+	    }
 	}
 }
