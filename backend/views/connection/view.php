3f4bf7aa980b969daffded76df155d729f58e444
Daniel <daniel.mikolajewski@wachowiakisyn.pl>
2018-02-05 09:17:58 +0100

    Zmiana widoku umowy

--- backend/views/connection/view.php
+++ backend/views/connection/view.php
@@ -1,27 +1,105 @@
 <?php
-use backend\models\Connection;
+
+use yii\bootstrap\Collapse;
+use yii\helpers\Html;
+use yii\widgets\DetailView;
 
 /**
  * @var yii\web\View $this
- * @var backend\models\Connection $model 
+ * @var backend\models\Connection $model
  */
+?>
  
- if ($model->type == 1 || $model->type == 3){
+<div class="col-sm-6" style="padding-left:0">
+
+    <?= DetailView::widget([
+        'model' => $model,
+        'formatter' => [
+            'class' => 'yii\i18n\Formatter',
+            'nullDisplay' => ''
+        ],
+        'attributes' => [
+            'id',
+            'soa_id',
+            'phone',
+            'phone2',
+            [
+                'attribute' => 'mac',
+                'visible' => in_array($model->type_id, [1,3]),
+            ],
+            [
+                'attribute' => 'vip',
+                'value' => $model->vip ? 'Tak' : 'Nie',
+                'contentOptions' => $model->vip ? ['style' => 'color: red'] : [],
+            ]
+        ],
+    ]);
+    ?>
+    
+</div>
+        
+<div class="col-sm-6" style="padding-right:0">
+
+	<?= DetailView::widget([
+    	'model' => $model,
+       	'formatter' => [
+        	'class' => 'yii\i18n\Formatter',
+        	'nullDisplay' => ''
+        ],
+        'attributes' => [
+            'start_date',
+        	'soa_date',
+            [
+                'attribute' => 'conf_date',
+                'visible' => in_array($model->type_id, [1,3]),
+            ],
+            [
+                'attribute' => 'phone_date',
+                'visible' => in_array($model->type_id, [2]),
+            ],
+            'pay_date',
+            'close_date',
+        ],
+    ]);
+    ?>
+    
+</div>
+
+<?= DetailView::widget([
+	'model' => $model,
+	'formatter' => [
+		'class' => 'yii\i18n\Formatter',
+		'nullDisplay' => ''
+	],
+	'attributes' => [
+		'info',
+		'info_boa'
+	],
+]);
+?>
+
+<?= Html::label('<h4>Instalacje : </h4>') ?> 
+    
+	<?php 
+	$i = 0;
+	$arIns = [];
 	
-	echo $this->renderAjax('_view_net', [
-		'model' => $model,
-	]);
-} elseif ($model->type == 2){
-	
-	echo $this->renderAjax('_view_phone', [
-			'model' => $model,
-	]);
-}
- 
+	foreach ($installations as $installation){
+		$arInstallations[$i]['label'] = $installation->type->name;
+	    $arInstallations[$i]['content'] = $this->render('@app/views/installation/_view', ['model' => $installation]);
+	    $i++;
+	}
+	 
+	if(isset($arInstallations))
+		echo Collapse::widget([
+	    	'items' => $arInstallations,
+		]); 
+	?>
+
+<?php 
 $this->registerJs(
 "$(function(){
-	$('.modal-header h4').html('{$model->modelAddress->toString()}');
+	$('.modal-header h4').html('{$model->address->toString()}');
 });"
 );
-?>   
-
+?>
