246cdbecb5843da9d24e64e6c6aa79e8c2ac934c
Daniel <daniel.mikolajewski@wachowiakisyn.pl>
2018-02-19 12:22:32 +0100

    Aktualizacja funkcji dodania urządzenia do magazynu

--- backend/controllers/StoreController.php
+++ backend/controllers/StoreController.php
@@ -2,21 +2,32 @@
 
 namespace backend\controllers;
 
-use Yii;
 use backend\models\Device;
 use backend\models\DeviceSearch;
+use Yii;
 use yii\base\Exception;
-use backend\models\DeviceFactory;
+use yii\filters\AjaxFilter;
 use yii\web\Controller;
+use yii\web\NotFoundHttpException;
 
-class StoreController extends Controller
-{ 
-    public function actionIndex()
-    {
+class StoreController extends Controller {
+    
+    public function behaviors() {
+        
+        return [
+            [
+                'class' => AjaxFilter::className(),
+                'only' => ['add']
+            ],
+        ];
+    }
+    
+    public function actionIndex() {
+
         $searchModel = new DeviceSearch();
         $dataProvider = $searchModel->search(Yii::$app->request->getQueryParams());
         
-        $dataProvider->query->andWhere(['address' => NULL]);
+        $dataProvider->query->andWhere(['address_id' => 1]);
 
         return $this->render('index', [
             'dataProvider' => $dataProvider,
@@ -24,39 +35,73 @@ class StoreController extends Controller
         ]);
     }
     
-    public function actionAdd($type)
-    {
-    	$modelDevice = DeviceFactory::create($type);   	
-    	$modelDevice->scenario = Device::SCENARIO_CREATE;
+    public function actionAdd($typeId) {
+        
+    	$device = Device::create($typeId);   	
+    	$device->scenario = Device::SCENARIO_CREATE;
            
         $request = Yii::$app->request;
         
-    	if ($request->isAjax){
-            if ($modelDevice->load($request->post())) {
+        if ($device->load($request->post())) {
+        	
+        	$device->status = null;
+        	$device->address_id = 1;
+        	
+            try {
+            	if(!$device->save()) throw new Exception('Problem z zapisem urządzenia');
             	
-            	$modelDevice->status = null;
-            	
-            	if($modelDevice->validate()){
-	                try {
-	                	if(!$modelDevice->save())
-	                		throw new Exception('Problem z zapisem urządzenia');
-	                	return 1;
-	                } catch (Exception $e) {
-	                	var_dump($modelDevice->errors);
-	                	var_dump($e->getMessage());
-	                	exit();
-	                }    
-	                
-            	} else {
-            		var_dump($modelDevice->errors);
-	               	exit();
-            	}
-            } else {
-                return $this->renderAjax('add', [
-                    'modelDevice' => $modelDevice,
-                ]);
+            	return 1;
+            } catch (Exception $e) {
+            	var_dump($device->errors);
+            	var_dump($e->getMessage());
+            	exit();
+            }    
+                
+        } else {
+            switch (get_class($device)){
+                case 'backend\models\Router':
+                    echo $this->renderAjax('add_router', [
+                    'device' => $device,
+                    ]);
+                    break;
+                case 'backend\models\Swith':
+                    
+                    echo $this->renderAjax('add_switch', [
+                    'device' => $device,
+                    ]);
+                    break;
+                case 'backend\models\GatewayVoip':
+                    
+                    echo $this->renderAjax('add_gateway_voip', [
+                    'device' => $device,
+                    ]);
+                    break;
+                case 'backend\models\Camera':
+                    
+                    echo $this->renderAjax('add_camera', [
+                    'device' => $device,
+                    ]);
+                    break;
+                case 'backend\models\Server':
+                    
+                    echo $this->renderAjax('add_server', [
+                    'device' => $device,
+                    ]);
+                    break;
+                case 'backend\models\Virtual':
+                    
+                    echo $this->renderAjax('add_virtual', [
+                    'device' => $device,
+                    ]);
+                    break;
+                case 'backend\models\MediaConverter':
+                    
+                    echo $this->renderAjax('add_media_converter', [
+                    'device' => $device,
+                    ]);
+                    break;
             }
-        } 
+        }
     }
     
     public function actionUpdate($id)
