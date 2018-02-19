71fd1fd0ea3fca1f64afb9b904bfba77c36e3a49
Daniel <daniel.mikolajewski@wachowiakisyn.pl>
2018-02-19 13:12:26 +0100

    Widok zamknięcia umowy

---
+++ backend/views/connection/close.php
@@ -0,0 +1,50 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+$form = ActiveForm::begin([
+	'id' => 'close'
+]) ?>
+    <p>Jeżeli jest to ostatnia umowa na tym hoście operacja dezaktywuje hosta i zwolni zajęty przez niego adres IP. Czy na pewno?</p>
+    
+    <?= Html::submitButton('Tak', ['class' => 'btn btn-success']) ?>
+  
+    <?= Html::button('Nie', ['class' => 'no btn btn-danger']) ?>
+
+<?php ActiveForm::end() ?>
+
+<?php
+$js = <<<JS
+$(function(){
+
+    $('.modal-header h4').html('Zamknij umowę');
+        
+    $('.no').click(function() {
+		$('#modal-device-sm').modal('hide');
+    });
+
+    $('#close').on('beforeSubmit', function(e){
+		
+		var form = $(this);
+	 	$.post(
+	  		form.attr('action'),
+	  		form.serialize()
+	 	).done(function(result){
+	 		if(result == 1){
+				$('#modal-device-sm').modal('hide');
+                //$('#device_desc').text('Odśwież...');
+	 		}
+	 		else{
+	 			console.log(result);
+	 		}
+	 	}).fail(function(){
+	 		console.log('server error');
+	 	});
+		return false;				
+	});	
+});
+JS;
+
+$this->registerJs($js);
+?>
