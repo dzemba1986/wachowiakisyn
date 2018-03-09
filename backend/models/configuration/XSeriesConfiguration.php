8ae5a5d7b2f374a756212f2d614ed5f82b5a0e60
Daniel <daniel.mikolajewski@wachowiakisyn.pl>
2018-03-09 14:26:51 +0100

    poprawka skryptu dla kamer

--- backend/models/configuration/XSeriesConfiguration.php
+++ backend/models/configuration/XSeriesConfiguration.php
@@ -19,146 +19,126 @@ class XSeriesConfiguration extends Configuration {
         if ($this->device instanceof Host) {
             $add = ' '; 
             if (strpos($this->device->parentIp, '172.') === 0) {
-$add = <<<ADD
-interface {$this->parentPortName}
-shutdown
-no switchport port-security
-switchport port-security violation protect
-switchport port-security maximum 0
-switchport port-security
-description {$this->desc}
-egress-rate-limit 508032k
-service-policy input 501M
-access-group anyuser
-switchport access vlan {$this->vlanId}
-spanning-tree portfast
-spanning-tree portfast bpdu-guard enable
-no shutdown
-exit
-mac address-table static {$this->mac} forward interface {$this->parentPortName} vlan {$this->vlanId}
-exit
-wr
-
-ADD;
+                $add = "interface {$this->parentPortName}\n";
+                $add .= "shutdown\n";
+                $add .= "no switchport port-security\n";
+                $add .= "switchport port-security violation protect\n";
+                $add .= "switchport port-security maximum 0\n";
+                $add .= "switchport port-security\n";
+                $add .= "description {$this->desc}\n";
+                $add .= "egress-rate-limit 508032k\n";
+                $add .= "service-policy input 501M\n";
+                $add .= "access-group anyuser\n";
+                $add .= "switchport access vlan {$this->vlanId}\n";
+                $add .= "spanning-tree portfast\n";
+                $add .= "spanning-tree portfast bpdu-guard enable\n";
+                $add .= "no shutdown\n";
+                $add .= "exit\n";
+                $add .= "mac address-table static {$this->mac} forward interface {$this->parentPortName} vlan {$this->vlanId}\n";
+                $add .= "exit\n";
+                $add .= "wr\n";
             } else {
                 if (count($this->device->connections) == 2) {
-$add = <<<ADD
-interface {$this->parentPortName}
-shutdown
-no switchport port-security
-switchport port-security violation protect
-switchport port-security maximum 0
-switchport port-security
-spanning-tree portfast
-spanning-tree portfast bpdu-guard enable
-description {$this->desc}
-egress-rate-limit 508032k
-service-policy input iptv-user-501M
-access-group iptv-user
-no ip igmp trusted all
-ip igmp trusted report
-switchport access vlan {$this->vlanId}
-no shutdown
-exit
-mac address-table static {$this->mac} forward interface {$this->parentPortName} vlan {$this->vlanId}
-exit
-wr
-
-ADD;
+                    $add = "interface {$this->parentPortName}\n";
+                    $add .= "shutdown\n";
+                    $add .= "no switchport port-security\n";
+                    $add .= "switchport port-security violation protect\n";
+                    $add .= "switchport port-security maximum 0\n";
+                    $add .= "switchport port-security\n";
+                    $add .= "spanning-tree portfast\n";
+                    $add .= "spanning-tree portfast bpdu-guard enable\n";
+                    $add .= "description {$this->desc}\n";
+                    $add .= "egress-rate-limit 508032k\n";
+                    $add .= "service-policy input iptv-user-501M\n";
+                    $add .= "access-group iptv-user\n";
+                    $add .= "no ip igmp trusted all\n";
+                    $add .= "ip igmp trusted report\n";
+                    $add .= "switchport access vlan {$this->vlanId}\n";
+                    $add .= "no shutdown\n";
+                    $add .= "exit\n";
+                    $add .= "mac address-table static {$this->mac} forward interface {$this->parentPortName} vlan {$this->vlanId}\n";
+                    $add .= "exit\n";
+                    $add .= "wr\n";
                 } elseif (count($this->device->connections) == 1) {
                     if ($this->device->connections[0]->type_id == 1) {
-$add = <<<ADD
-interface {$this->parentPortName}
-shutdown
-no switchport port-security
-switchport port-security violation protect
-switchport port-security maximum 0
-switchport port-security
-spanning-tree portfast
-spanning-tree portfast bpdu-guard enable
-description {$this->desc}
-egress-rate-limit 508032k
-service-policy input internet-user-501M
-access-group internet-user
-no ip igmp trusted all
-ip igmp trusted report
-switchport access vlan {$this->vlanId}
-no shutdown
-exit
-mac address-table static {$this->mac} forward interface {$this->parentPortName} vlan {$this->vlanId}
-exit
-wr
-
-ADD;
+                        $add = "interface {$this->parentPortName}\n";
+                        $add .= "shutdown\n";
+                        $add .= "no switchport port-security\n";
+                        $add .= "switchport port-security violation protect\n";
+                        $add .= "switchport port-security maximum 0\n";
+                        $add .= "switchport port-security\n";
+                        $add .= "spanning-tree portfast\n";
+                        $add .= "spanning-tree portfast bpdu-guard enable\n";
+                        $add .= "description {$this->desc}\n";
+                        $add .= "egress-rate-limit 508032k\n";
+                        $add .= "service-policy input internet-user-501M\n";
+                        $add .= "access-group internet-user\n";
+                        $add .= "no ip igmp trusted all\n";
+                        $add .= "ip igmp trusted report\n";
+                        $add .= "switchport access vlan {$this->vlanId}\n";
+                        $add .= "no shutdown\n";
+                        $add .= "exit\n";
+                        $add .= "mac address-table static {$this->mac} forward interface {$this->parentPortName} vlan {$this->vlanId}\n";
+                        $add .= "exit\n";
+                        $add .= "wr\n";
                     } elseif ($this->device->connections[0]->type_id == 3) {
-$add = <<<ADD
-interface {$this->parentPortName}
-shutdown
-no switchport port-security
-switchport port-security violation protect
-switchport port-security maximum 0
-switchport port-security
-spanning-tree portfast
-spanning-tree portfast bpdu-guard enable
-description {$this->desc}
-egress-rate-limit 508032k
-service-policy input iptv-only-501M
-access-group iptv-only
-no ip igmp trusted all
-ip igmp trusted report
-switchport access vlan {$this->vlanId}
-no shutdown
-exit
-mac address-table static {$this->mac} forward interface {$this->parentPortName} vlan {$this->vlanId}
-exit
-wr
-
-ADD;
+                        $add = "interface {$this->parentPortName}\n";
+                        $add .= "shutdown\n";
+                        $add .= "no switchport port-security\n";
+                        $add .= "switchport port-security violation protect\n";
+                        $add .= "switchport port-security maximum 0\n";
+                        $add .= "switchport port-security\n";
+                        $add .= "spanning-tree portfast\n";
+                        $add .= "spanning-tree portfast bpdu-guard enable\n";
+                        $add .= "description {$this->desc}\n";
+                        $add .= "egress-rate-limit 508032k\n";
+                        $add .= "service-policy input iptv-only-501M\n";
+                        $add .= "access-group iptv-only\n";
+                        $add .= "no ip igmp trusted all\n";
+                        $add .= "ip igmp trusted report\n";
+                        $add .= "switchport access vlan {$this->vlanId}\n";
+                        $add .= "no shutdown\n";
+                        $add .= "exit\n";
+                        $add .= "mac address-table static {$this->mac} forward interface {$this->parentPortName} vlan {$this->vlanId}\n";
+                        $add .= "exit\n";
+                        $add .= "wr\n";
                     }
                 }
             }
         } elseif ($this->device instanceof GatewayVoip) {
-
-$add = <<<ADD
-access-list hardware voip{$this->parentPortNumber}
-deny udp any any eq 68
-permit ip {$this->ip} 0.0.0.0 213.5.208.0 0.0.0.63
-permit ip {$this->ip} 0.0.0.0 213.5.208.128 0.0.0.63
-permit ip {$this->ip} 0.0.0.0 10.111.0.0 0.0.255.255
-permit udp 0.0.0.0 0.0.0.0 eq 68 any eq 67
-deny ip any any
-exit
-interface {$this->parentPortName}
-shutdown
-description {$this->desc}
-switchport access vlan {$this->vlanId}
-access-group voip{$this->parentPortNumber}
-spanning-tree portfast
-spanning-tree portfast bpdu-guard enable
-no shutdown
-exit
-exit
-wr
-
-ADD;
+            $add = "access-list hardware voip{$this->parentPortNumber}\n";
+            $add .= "deny udp any any eq 68\n";
+            $add .= "permit ip {$this->ip} 0.0.0.0 213.5.208.0 0.0.0.63\n";
+            $add .= "permit ip {$this->ip} 0.0.0.0 213.5.208.128 0.0.0.63\n";
+            $add .= "permit ip {$this->ip} 0.0.0.0 10.111.0.0 0.0.255.255\n";
+            $add .= "permit udp 0.0.0.0 0.0.0.0 eq 68 any eq 67\n";
+            $add .= "deny ip any any\n";
+            $add .= "exit\n";
+            $add .= "interface {$this->parentPortName}\n";
+            $add .= "shutdown\n";
+            $add .= "description {$this->desc}\n";
+            $add .= "switchport access vlan {$this->vlanId}\n";
+            $add .= "access-group voip{$this->parentPortNumber}\n";
+            $add .= "spanning-tree portfast\n";
+            $add .= "spanning-tree portfast bpdu-guard enable\n";
+            $add .= "no shutdown\n";
+            $add .= "exit\n";
+            $add .= "exit\n";
+            $add .= "wr\n";
         } elseif ($this->device instanceof Camera) {
-
-$add = <<<ADD
-interface {$this->parentPortName}
-shutdown
-description {$this->desc}
-switchport access vlan {$this->vlanId}
-access-group camera
-switchport port-security violation protect
-switchport port-security maximum 0
-switchport port-security
-no shutdown
-exit
-mac address-table static {$this->mac} forward interface port1.0.1 vlan 3
-exit
-wr
-
-ADD;
+            $add = "interface {$this->parentPortName}\n";
+            $add .= "shutdown\n";
+            $add .= "description {$this->desc}\n";
+            $add .= "switchport access vlan {$this->vlanId}\n";
+            $add .= "access-group camera\n";
+            $add .= "switchport port-security violation protect\n";
+            $add .= "switchport port-security maximum 0\n";
+            $add .= "switchport port-security\n";
+            $add .= "no shutdown\n";
+            $add .= "exit\n";
+            $add .= "mac address-table static {$this->mac} forward interface {$this->parentPortName} vlan {$this->vlanId}\n";
+            $add .= "exit\n";
+            $add .= "wr\n";
         }
 
     return $add;
@@ -168,102 +148,79 @@ ADD;
         $drop = ' '; 
         if ($this->device instanceof Host) {
             if (strpos($this->device->parentIp, '172.') === 0) {
-$drop = <<<DELETE
-no mac address-table static {$this->mac} forward interface {$this->parentPortName} vlan {$this->vlanId}
-interface {$this->parentPortName}
-no switchport port-security
-no service-policy input 501M
-no egress-rate-limit
-no access-group anyuser
-switchport access vlan 555
-exit
-do clear ip dhcp snooping binding int {$this->parentPortName}
-exit
-wr
-
-DELETE;
+                $drop = "no mac address-table static {$this->mac} forward interface {$this->parentPortName} vlan {$this->vlanId}\n";
+                $drop .= "interface {$this->parentPortName}\n";
+                $drop .= "no switchport port-security\n";
+                $drop .= "no service-policy input 501M\n";
+                $drop .= "no egress-rate-limit\n";
+                $drop .= "no access-group anyuser\n";
+                $drop .= "switchport access vlan 555\n";
+                $drop .= "exit\n";
+                $drop .= "do clear ip dhcp snooping binding int {$this->parentPortName}\n";
+                $drop .= "exit\n";
+                $drop .= "wr\n";
             } else {
-$drop = <<<DELETE
-no mac address-table static {$this->mac} forward interface {$this->parentPortName} vlan {$this->vlanId}
-int {$this->parentPortName}
-no switchport port-security
-no service-policy input internet-user-501M
-no service-policy input iptv-user-501M
-no service-policy input iptv-only-501M
-no egress-rate-limit
-no ip igmp trust all
-no access-group internet-user
-no access-group iptv-user
-no access-group iptv-only
-switchport access vlan 555
-exit
-do clear ip dhcp snooping binding interface {$this->parentPortName}
-exit
-
-DELETE;
+                $drop = "no mac address-table static {$this->mac} forward interface {$this->parentPortName} vlan {$this->vlanId}\n";
+                $drop .= "int {$this->parentPortName}\n";
+                $drop .= "no switchport port-security\n";
+                $drop .= "no service-policy input internet-user-501M\n";
+                $drop .= "no service-policy input iptv-user-501M\n";
+                $drop .= "no service-policy input iptv-only-501M\n";
+                $drop .= "no egress-rate-limit\n";
+                $drop .= "no ip igmp trust all\n";
+                $drop .= "no access-group internet-user\n";
+                $drop .= "no access-group iptv-user\n";
+                $drop .= "no access-group iptv-only\n";
+                $drop .= "switchport access vlan 555\n";
+                $drop .= "exit\n";
+                $drop .= "do clear ip dhcp snooping binding interface {$this->parentPortName}\n";
+                $drop .= "exit\n";
             }
 
         } elseif ($this->device instanceof GatewayVoip) {
-
-$drop = <<<DELETE
-interface {$this->parentPortName}
-shutdown
-no access-group voip{$this->parentPortNumber}
-switchport access vlan 555
-no shutdown
-exit
-no access-list hardware voip{$this->parentPortNumber}
-exit
-wr
-
-DELETE;
-            
+            $drop = "interface {$this->parentPortName}\n";
+            $drop .= "shutdown\n";
+            $drop .= "no access-group voip{$this->parentPortNumber}\n";
+            $drop .= "switchport access vlan 555\n";
+            $drop .= "no shutdown\n";
+            $drop .= "exit\n";
+            $drop .= "no access-list hardware voip{$this->parentPortNumber}\n";
+            $drop .= "exit\n";
+            $drop .= "wr\n";
         } elseif ($this->device instanceof Camera) {
-
-$drop = <<<DELETE
-no mac address-table static {$this->mac} forward interface {$this->parentPortName} vlan {$this->vlanId}
-interface {$this->parentPortName}
-shutdown
-no access-group camera
-no description
-no switchport port-security
-switchport access vlan 555
-no shutdown
-exit
-exit
-wr
-
-DELETE;
-            
+            $drop = "no mac address-table static {$this->mac} forward interface {$this->parentPortName} vlan {$this->vlanId}\n";
+            $drop .= "interface {$this->parentPortName}\n";
+            $drop .= "shutdown\n";
+            $drop .= "no access-group camera\n";
+            $drop .= "no description\n";
+            $drop .= "no switchport port-security\n";
+            $drop .= "switchport access vlan 555\n";
+            $drop .= "no shutdown\n";
+            $drop .= "exit\n";
+            $drop .= "exit\n";
+            $drop .= "wr\n";
         }
-        
     return $drop;
     }
     
     function changeMac($newMac) {
-        
         $newMac = preg_replace('/^([A-Fa-f0-9]{4})([A-Fa-f0-9]{4})([A-Fa-f0-9]{4})$/', '$1.$2.$3', str_replace([':', '.', '-'], '', $newMac));
-        
         if ($this->device instanceof Host) {
-
-$change = <<<CHANGE
-interface {$this->parentPortName}
-shutdown
-no switchport port-security
-exit
-no mac address-table static {$this->mac} forward interface {$this->parentPortName} vlan {$this->vlanId}
-mac address-table static {$newMac} forward interface {$this->parentPortName} vlan {$this->vlanId}
-exit
-clear ip dhcp snooping binding {$this->ip}
-configure terminal
-interface {$this->parentPortName}
-switchport port-security
-no shutdown
-exit
-exit
-wr	
-
-CHANGE;
+            $change = "interface {$this->parentPortName}\n";
+            $change .= "shutdown\n";
+            $change .= "no switchport port-security\n";
+            $change .= "exit\n";
+            $change .= "no mac address-table static {$this->mac} forward interface {$this->parentPortName} vlan {$this->vlanId}\n";
+            $change .= "mac address-table static {$newMac} forward interface {$this->parentPortName} vlan {$this->vlanId}\n";
+            $change .= "exit\n";
+            $change .= "clear ip dhcp snooping binding {$this->ip}\n";
+            $change .= "configure terminal\n";
+            $change .= "interface {$this->parentPortName}\n";
+            $change .= "switchport port-security\n";
+            $change .= "no shutdown\n";
+            $change .= "exit\n";
+            $change .= "exit\n";
+            $change .= "wr\n";	
         } else $change = ' ';
 
     return $change;
