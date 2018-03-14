55e25200967e8353537bf3b4773545a5ce5ef11a
Daniel <daniel.mikolajewski@wachowiakisyn.pl>
2018-03-14 13:59:32 +0100

    sprawdzanie wyboru portu
    po wybraniu portu następuje sprawdzanie czy dany port nie jest już
    wybrany dla innego urządzenia - dziecka.

--- backend/views/tree/replace_port.php
+++ backend/views/tree/replace_port.php
@@ -7,7 +7,7 @@ use yii\helpers\Url;
  * @var backend\models\Tree[] $links
  * @var backend\models\Device $sourceDevice
  * @var integer $destinationDeviceId
- * @var $onetoone
+ * @var boolean $onetoone
  */
 
 echo "<div class='row'>";
@@ -19,16 +19,16 @@ echo '</div>';
 echo "<div class='row'>";
 foreach ($links as $link) {
     
-    $string = Device::findOne($link['device'])->address->toString(true);
-    $dropDown = Html::dropDownList('map[' . $link['port'] . ']',  null, [], ['class' => 'port form-control', 'port' => $link['port']]);
+    $string = Device::findOne($link['device'])->name;
+    $dropDown = Html::dropDownList("map[{$link['port']}]",  null, [], ['class' => 'port form-control', 'port' => $link['port']]);
     echo "<div class='col-md-4'>";
     echo "{$sourceDevice->model->port[$link['port']]} - {$string} $dropDown";
     echo '</div>';
 }
 echo '</div>';
-
+echo '</br>';
 echo '<div class="form-group">';
-echo Html::submitButton('Zamień', ['id' => 'change', 'class' => 'btn btn-primary']);
+echo Html::submitButton('Zamień', ['class' => 'change btn btn-primary']);
 echo '</div>';
 
 $urlPortList = Url::to(['tree/list-port']);
@@ -39,14 +39,34 @@ $(function(){
 	});
 
     $('.port').on('change', function () {
-        var val = $(this).val();
-        var i = $(this).index();
-        $('.port:not(:eq('+i+'))').each(function () {
-            if ($(this).val() === val) {
-                alert($(this).index()+' has the same value');
-            }
+        $('.port').each(function () {
+            var val = $(this).val();
+            var i = $(this).index('.port');
+            $('.port:not(:eq(' + i + '))').each(function () {
+                if ($(this).val() == val) {
+                    $('.port').eq($(this).index('.port')).css('border-color', '#a94442');
+                    $('.port').eq(i).css('border-color', '#a94442');
+                } else { 
+                    $('.port').eq(i).css('border-color', '');
+                }
+            });
         });
     });
+
+//     $('.port').on('change', function () {
+//         var val = $(this).val();
+//         var i = $(this).index('.port');
+//         $('.port').eq(i).css('border-color', '');
+//         $('.port:not(:eq(' + i + '))').each(function () {
+//             if ($(this).val() == val) {
+//                 //alert($(this).index('.port') + ' ma tą samą wartość');
+//                 $('.port').eq($(this).index('.port')).css('border-color', '#a94442');
+//                 $('.port').eq(i).css('border-color', '#a94442');
+//             } else { 
+//                 $('.port').eq($(this).index('.port')).css('border-color', '');
+//             }
+//         });
+//     });
 
     $('input[name="onetoone"]').change(function() {
         if(this.checked) {
