b0ab65783693b9161a38036b6907bffefc7089fc
Daniel <daniel.mikolajewski@wachowiakisyn.pl>
2018-06-16 12:16:09 +0200

    widok podmiany portów

--- backend/views/tree/replace_port.php
+++ backend/views/tree/replace_port.php
@@ -10,11 +10,8 @@ use yii\helpers\Url;
  * @var boolean $onetoone
  */
 
-echo "<div class='row'>";
-echo "<div class='col-md-6'>";
 echo Html::checkbox('onetoone', false, ['label' => '"1 do 1"', 'disabled' => !$onetoone]);
-echo '</div>';
-echo '</div>';
+echo '<div class="help-block"></div>';
 
 echo "<div class='row'>";
 foreach ($links as $link) {
@@ -22,11 +19,11 @@ foreach ($links as $link) {
     $string = Device::findOne($link['device'])->name;
     $dropDown = Html::dropDownList("map[{$link['port']}]",  null, [], ['class' => 'port form-control', 'port' => $link['port']]);
     echo "<div class='col-md-4'>";
-    echo "{$sourceDevice->model->port[$link['port']]} - {$string} $dropDown";
+    echo "{$sDevice->model->port[$link['port']]} - {$string} $dropDown";
+    echo '<div class="help-block"></div>';
     echo '</div>';
 }
 echo '</div>';
-echo '</br>';
 echo '<div class="form-group">';
 echo Html::submitButton('Zamień', ['class' => 'change btn btn-primary']);
 echo '</div>';
@@ -34,7 +31,7 @@ echo '</div>';
 $urlPortList = Url::to(['tree/list-port']);
 $js = <<<JS
 $(function(){
-    $.get('{$urlPortList}&deviceId=' + {$destinationDeviceId} + '&install=true&mode=all', function(data){
+    $.get('{$urlPortList}&deviceId=' + {$dId} + '&install=true&mode=all', function(data){
 		$('.port').html(data);
 	});
 
@@ -52,21 +49,6 @@ $(function(){
             });
         });
     });
-
-//     $('.port').on('change', function () {
-//         var val = $(this).val();
-//         var i = $(this).index('.port');
-//         $('.port').eq(i).css('border-color', '');
-//         $('.port:not(:eq(' + i + '))').each(function () {
-//             if ($(this).val() == val) {
-//                 //alert($(this).index('.port') + ' ma tą samą wartość');
-//                 $('.port').eq($(this).index('.port')).css('border-color', '#a94442');
-//                 $('.port').eq(i).css('border-color', '#a94442');
-//             } else { 
-//                 $('.port').eq($(this).index('.port')).css('border-color', '');
-//             }
-//         });
-//     });
 
     $('input[name="onetoone"]').change(function() {
         if(this.checked) {
