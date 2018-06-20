9fc6952245b06f88d8ad71f30346b0da128ac2be
Daniel <daniel.mikolajewski@wachowiakisyn.pl>
2018-06-20 11:45:49 +0200

    nowy typ urządzenia : UPS

---
+++ backend/models/Ups.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace backend\models;
+
+use vakorovin\yii2_macaddress_validator\MacaddressValidator;
+use yii\helpers\ArrayHelper;
+
+class Ups extends Device
+{
+	const TYPE = 10;
+	
+	public function init() {
+	    
+		$this->type_id = self::TYPE;
+		parent::init();
+	}
+	
+	public static function find() {
+	    
+		return new DeviceQuery(get_called_class(), ['type_id' => self::TYPE]);
+	}
+	
+	public function beforeSave($insert) {
+	    
+		if (!$insert)  $this->type_id = self::TYPE;
+		
+		return parent::beforeSave($insert);
+	}
+	
+	public function rules() {
+	    
+	    return ArrayHelper::merge(
+	        parent::rules(),
+	        [
+	            ['mac', 'required', 'message' => 'Wartość wymagana'],
+	            ['mac', MacaddressValidator::className(), 'message' => 'Zły format'],
+	            
+	            ['serial', 'required', 'message' => 'Wartość wymagana'],
+	            
+	            ['manufacturer_id', 'required', 'message' => 'Wartość wymagana'],
+	            
+	            ['model_id', 'required', 'message' => 'Wartość wymagana'],
+	            
+	            [['serial', 'manufacturer_id', 'model_id'], 'safe'],
+	        ]
+		);
+	}
+	
+	public function scenarios() {
+	    
+	    $scenarios = parent::scenarios();
+	    $scenarios[self::SCENARIO_CREATE] = ArrayHelper::merge($scenarios[self::SCENARIO_UPDATE],['serial', 'manufacturer_id', 'model_id', 'mac']);
+	    $scenarios[self::SCENARIO_UPDATE] = ArrayHelper::merge($scenarios[self::SCENARIO_UPDATE], ['serial', 'mac']);
+	    
+	    return $scenarios;
+	}
+}
---
+++ backend/views/ups/view.php
@@ -0,0 +1,37 @@
+<?php
+use yii\widgets\DetailView;
+
+/**
+ * @var backend\models\MediaConverter $device
+ */
+
+echo '<div class="col-md-5">';
+echo DetailView::widget([
+	'model' => $device,
+	'attributes' => [
+		'id',	
+		[
+			'label' => 'Adres',
+			'value' => $device->address->toString()
+		],
+		[
+			'label' => 'Status',
+			'value' => $device->status ? 'Aktywny' : 'Nieaktywny'
+		],
+		[
+			'label' => 'Typ',
+			'value' => $device->type->name
+		],
+		'serial',
+		[
+			'label' => 'Model',
+			'value' => $device->model->name,
+		],
+		[
+			'label' => 'Producent',
+			'value' => $device->manufacturer->name,
+		],
+	]
+]);
+echo '</div>';
+?>
