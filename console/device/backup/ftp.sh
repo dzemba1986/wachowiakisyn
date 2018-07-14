6cf4e9ec1ee45a598c01d7217f39411bd9b0954a
Daniel <daniel.mikolajewski@wachowiakisyn.pl>
2018-07-14 23:30:15 +0200

    plik crona

--- console/device/backup/ftp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-pathToTftp="/var/tftp";
-
-mkdir $pathToTftp/$(date +%Y-%m-%d)
-sleep 3
-mv $pathToTftp/*.rtf $pathToTftp/$(date +%Y-%m-%d)
-lftp -e "set ssl:verify-certificate no; mirror -R /var/tftp/$(date +%Y-%m-%d)/ /switch/; exit" -p 2121 -u backup,b@c4@p 10.111.233.2
