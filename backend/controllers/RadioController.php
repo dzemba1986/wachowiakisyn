f17291e2a23cd91067ff78c902861d4431f192f4
Daniel <daniel.mikolajewski@wachowiakisyn.pl>
2018-08-29 11:11:17 +0200

    nowy typ urządzenia - radiolinia

---
+++ backend/controllers/RadioController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace backend\controllers;
+
+use backend\models\Radio;
+
+class RadioController extends DeviceController {
+    
+    protected static function getModel() {
+        
+        return new Radio();
+    }
+}
