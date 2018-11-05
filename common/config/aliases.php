664280ca3ce1d8b5d458b78dfc0bd1967f1fe7ac
Daniel <daniel.mikolajewski@wachowiakisyn.pl>
2018-11-05 09:10:14 +0100

    dodanie aliasów

--- common/config/aliases.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-Yii::setAlias('common', dirname(__DIR__));
-Yii::setAlias('backend', dirname(dirname(__DIR__)) . '/backend');
-Yii::setAlias('rest', dirname(dirname(__DIR__)) . '/rest');
-Yii::setAlias('console', dirname(dirname(__DIR__)) . '/console');
