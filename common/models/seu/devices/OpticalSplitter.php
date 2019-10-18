be8d02cc2be68b158e859a4ae2f7d80261c1c99d
Daniel Mikołajewski <daniel.mikolajewski@wachowiakisyn.pl>
2019-10-18 13:16:01 +0200

    Dodanie tłumienia do nowego modelu splitera

--- common/models/seu/devices/OpticalSplitter.php
+++ common/models/seu/devices/OpticalSplitter.php
@@ -53,7 +53,7 @@ class OpticalSplitter extends BusinessDevice
 	                if ($model->model_id == 36) $out = 3.8;
 	                if ($model->model_id == 37) $out = 7.2;
 	                if ($model->model_id == 38 || $model->model_id == 82) $out = 10.2;
-	                if ($model->model_id == 39) $out = 13.6;
+	                if ($model->model_id == 39 || $model->model_id == 88) $out = 13.6;
 	                if ($model->model_id == 44) $out = 16.8;
 	                if ($model->model_id == 65) $out = 20.9;
 	                
